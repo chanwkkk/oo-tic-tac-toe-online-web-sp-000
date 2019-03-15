c3a3f3abd0a240ce215fc0170111c16cc2eb4e2a
Yingqi Chen <chanwk18@hotmail.com>
2019-03-15 18:03:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -52,6 +52,25 @@ def turn
       display_board
   else turn
   end
+end
+
+def  turn_count
+  count=0
+  board.each do |value|
+    if !(value==" ")
+      count+=1
+  end
+  end
+  return count
+  end
+
+def current_player(board)
+  turns=turn_count(board)
+  turns.even?? "X": "O"
+  # if turns%2==0
+   #    "X"
+   #else "O"
+   # end
 
 end
 
