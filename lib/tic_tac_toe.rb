54fb76e5d6739ebd88f1faab3952f4b796062b19
Yingqi Chen <chanwk18@hotmail.com>
2019-03-15 17:56:56 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,4 +21,46 @@ def display_board
   puts "-----------"
   puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
 end
+
+def input_to_index(input)
+  if input.to_i.to_s==input
+     return input.to_i-1
+   else return -1
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
